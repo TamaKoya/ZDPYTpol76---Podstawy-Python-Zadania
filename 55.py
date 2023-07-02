f34fdac17f689490dd41477ceb897393e21cc32e
TamaKoya <dinosz99@gmail.com>
2023-07-02 14:39:29 +0200

    Rozwiązane zadania z instrukcji if, else, elif << 02.07 >>

---
+++ 55.py
@@ -0,0 +1,5 @@
+name = input("Hello there, please say your name: ")
+if name == "Bond":
+    print("Welcome on board, 007")
+else:
+    print("Good morning, " + name)
---
+++ 56.py
@@ -0,0 +1,5 @@
+liczba = input("Podaj liczbę: ")
+if int(liczba) % 2 == 0:
+    print("Liczba jest parzysta")
+else:
+    print("Liczba jest nieparzysta")
